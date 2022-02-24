d3547244a8f3b8c766eef273e7ba6640c85639bf
msfm2018 <msfm@163.com>
2022-02-24 10:22:33 +0800

    up

--- Unit1.pas
+++ Unit1.pas
@@ -16,69 +16,62 @@ type
     procedure Timer1Timer(Sender: TObject);
     procedure Button1Click(Sender: TObject);
     procedure Timer2Timer(Sender: TObject);
-  private
-    { Private declarations }
-  public
-    { Public declarations }
+
   end;
 
 var
-g_parent:thandle;
+  g_top: thandle;
   Form1: TForm1;
-   grect: trect;
+  grect: trect;
   gold_left: integer;
   rt: trect;
   sname: string;
   gh: thandle;
-  x,y,lParamxx:Cardinal;    lp:trect;    pp:tpoint;
+  x, y, lParamxx: Cardinal;
+  lp: trect;
+  pp: tpoint;
+
 implementation
 
 {$R *.dfm}
-       function EnumChildWndProc(AhWnd:LongInt;
-AlParam:lParam):boolean;stdcall;
+function EnumChildWndProc(AhWnd: LongInt; AlParam: lParam): boolean; stdcall;
 var
-WndClassName: array[0..254] of Char;
-WndCaption: array[0..254] of Char;
+  WndClassName: array[0..254] of Char;
+  WndCaption: array[0..254] of Char;
 begin
-GetClassName(AhWnd,wndClassName,254);
-GetWindowText(aHwnd,WndCaption,254);
-if STRING(WndCaption)='XP1' then    //复活按钮
-                           BEGIN
-                                  SendMessage(ahwnd,WM_LBUTTONDOWN,0,0);
-                                                    SendMessage(ahwnd,WM_LBUTTONUP,0,0);
-                           END;
+  GetClassName(AhWnd, WndClassName, 254);
+  GetWindowText(AhWnd, WndCaption, 254);
+  if string(WndCaption) = 'XP1' then    //复活按钮
+  begin
+    SendMessage(AhWnd, WM_LBUTTONDOWN, 0, 0);
+    SendMessage(AhWnd, WM_LBUTTONUP, 0, 0);
+  end;
 
-
-
-                           if string(wndcaption)='MPC物品栏' then    // 天石包在背包第一位置 起到捡钱目的
-                           begin
-                      GetClientRect(ahwnd,lp)  ;
-                                pp.X:=lp.Left;
-                                pp.Y:=lp.Top;
+  if string(WndCaption) = 'MPC物品栏' then    // 天石包在背包第一位置 起到捡钱目的
+  begin
+    GetClientRect(AhWnd, lp);
+    pp.X := lp.Left;
+    pp.Y := lp.Top;
                           //   ClientToScreen(ahwnd,pp)  ;
             //    SetCursorPos(pp.X+20,pp.y+20)
 
-              x:=lp.Left+20;//x坐标，
-              y:=lp.Top+20;//y坐标，
-              lParamxx:=(y shl 16) or x;     //  高低16位各代表意义
-              SendMessage(ahwnd, WM_RBUTTONDOWN, 0, lParamxx);
-              SendMessage(ahwnd, WM_RBUTTONUP, 0, lParamxx);
-                           end;
+    x := lp.Left + 20; //x坐标，
+    y := lp.Top + 20; //y坐标，
+    lParamxx := (y shl 16) or x;     //  高低16位各代表意义
+    SendMessage(AhWnd, WM_RBUTTONDOWN, 0, lParamxx);
+    SendMessage(AhWnd, WM_RBUTTONUP, 0, lParamxx);
+  end;
 
-result:=true;
+  result := true;
 end;
+
 procedure TForm1.FormShow(Sender: TObject);
 var
   h: HWnd;
   p: array[0..254] of char;
-var
-  FPos: tpoint;
-var
-  lt, rb: tpoint;
-  bmp: tbitmap;
-  pixel: TPixelFormat;  ss:string;
+  ss: string;
 begin
-g_parent:=0;
+  g_top := 0;
   gh := 0;
   h := GetWindow(Handle, GW_HWNDFIRST);
   while h <> 0 do
@@ -87,48 +80,43 @@ g_parent:=0;
     begin
 
       sname := p;
-      ss:='美眉征服';
-      if pos( (ss),(sname))>0 then
+      ss := '美眉征服';
+      if pos((ss), (sname)) > 0 then
       begin
-      g_parent:=h;
+        g_top := h;
         gh := FindWindowEx(h, 0, '#32770', nil);
         if gh <> 0 then
           caption := 'zhaodao';
-          break;
+        break;
       end;
 
-    //  Memo1.Lines.Add(p);
     end;
     h := GetWindow(h, GW_HWNDNEXT);
   end;
-
 
 end;
 
 procedure TForm1.Timer1Timer(Sender: TObject);
 begin
-EnumChildWindows(gh,@EnumChildWndProc,0);
+  EnumChildWindows(gh, @EnumChildWndProc, 0);
 end;
 
 procedure TForm1.Button1Click(Sender: TObject);
 begin
-   EnumChildWindows(gh,@EnumChildWndProc,0);
+  EnumChildWindows(gh, @EnumChildWndProc, 0);
 end;
 
 procedure TForm1.Timer2Timer(Sender: TObject);
 begin
-        if g_parent<>0 then          //底部快捷键根据需要设置
-                           begin
-                           SetForegroundWindow( g_parent );
+  if g_top <> 0 then          //底部快捷键根据需要设置
+  begin
+    SetForegroundWindow(g_top);
 
-
-keybd_event(VK_F2, 0, 0, 0);
-  keybd_event(VK_F2, 0, KEYEVENTF_KEYUP, 0);
-//timer2.Enabled:=false;
-end;
+    keybd_event(VK_F2, 0, 0, 0);
+    keybd_event(VK_F2, 0, KEYEVENTF_KEYUP, 0);
+  end;
 
 end;
-
-
 
 end.
+
